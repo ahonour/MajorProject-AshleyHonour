ae142bbf23058dd45c648cc44cb6531195b124aa
ahonour <ahonour@my.bcit.ca>
2024-10-26 17:05:17 -0700

    Enemy creation

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,28 @@ const game = {
   enemyUnits: [],
   DOM: document.querySelector('#game'),
   playerDOM: document.querySelector('#playerArea'),
+  enemyDOM: document.querySelector('#enemyArea'),
   addNewUnit(newUnit, allgeiance) {
     if (allgeiance === 'ally') {
       this.playerUnits.push(newUnit);
       const playerNum = this.playerUnits.length - 1;
-      const PlayerName = `<div class="unitName">${newUnit.name}</div>`;
-      const currentHP = `<div id='playerUnitCurrentHP${
-        this.playerUnits.length - 1
-      }'>${newUnit.currentHP}</div>`;
-      const PlayerHP = `<div class="unitName">Health: ${currentHP}/${newUnit.totalHP}</div>`;
-      const PlayerInfo = `<div class="unitInfo">${PlayerName}${PlayerHP}</div>`;
-      const PlayerDice = `<div class="playerDice" id='playerDice${playerNum}'></div>`;
-      const newUnitTotal = `<div class="playerUnit" id='playerUnit${playerNum}'>${PlayerInfo}${PlayerDice}</div>`;
+      const playerName = `<div class="unitName">${newUnit.name}</div>`;
+      const currentHP = `<div id='playerUnitCurrentHP${playerNum}'>${newUnit.currentHP}</div>`;
+      const playerHP = `<div>Health: ${currentHP}/${newUnit.totalHP}</div>`;
+      const playerInfo = `<div class="unitInfo">${playerName}${playerHP}</div>`;
+      const playerDice = `<div class="playerDice" id='playerDice${playerNum}'></div>`;
+      const newUnitTotal = `<div class="playerUnit" id='playerUnit${playerNum}'>${playerInfo}${playerDice}</div>`;
       this.playerDOM.innerHTML += newUnitTotal;
     } else {
       this.enemyUnits.push(newUnit);
+      const enemyNum = this.enemyUnits.length - 1;
+      const enemyName = `<div class="unitName">${newUnit.name}</div>`;
+      const currentHP = `<div id='enemnyUnitCurrentHP${enemyNum}'>${newUnit.currentHP}</div>`;
+      const enemyHP = `<div>Health: ${currentHP}/${newUnit.totalHP}</div>`;
+      const enemyInfo = `<div class="unitInfo">${enemyName}${enemyHP}</div>`;
+      const enemyDice = `<div class="enemyDice" id='enemyDice${enemyNum}'></div>`;
+      const newUnitTotal = `<div class="enemyUnit" id='enemyUnit${enemyNum}'>${enemyInfo}${enemyDice}</div>`;
+      this.enemyDOM.innerHTML += newUnitTotal;
     }
   },
 };
@@ -33,5 +40,15 @@ class PlayerUnit {
     this.totalHP = health;
     this.diceSides = diceSides; //Make a function to randomly generate
     game.addNewUnit(this, 'ally');
+  }
+}
+
+class EnemyUnit {
+  constructor(name, health, diceSides) {
+    this.name = name;
+    this.currentHP = health;
+    this.totalHP = health;
+    this.diceSides = diceSides; //Make a function to randomly generate
+    game.addNewUnit(this, 'foe');
   }
 }
